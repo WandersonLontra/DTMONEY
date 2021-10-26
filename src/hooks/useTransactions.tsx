649b7d9480746b06d84e330ef2964fb3d3d66834
WandersonLontra <wandersonlontra@yahoo.com.br>
2021-10-26 16:44:20 -0300

    Store in GitHub

---
+++ src/hooks/useTransactions.tsx
@@ -0,0 +1,68 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+interface Transaction {
+    id: number;
+    title: string;
+    type: string;
+    amount: number;
+    category: string;
+    createdAt: string;
+}
+
+interface TransactionsProviderProps {
+    children: ReactNode;
+}
+
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+
+const TransactionsContext = createContext({} as TransactionsContextData);
+
+
+export function TransactionsProvider({children}: TransactionsProviderProps) {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    useEffect(() => {
+        api.get('/transactions')
+            .then((response: any) => {
+                setTransactions(response.data.transactions);
+            })
+    }, []);
+
+    async function createTransaction(transactionInput: TransactionInput){
+
+        const {data} = await api.post<any>('/transactions', {
+            ...transactionInput,
+            createdAt: new Date()
+        });
+
+        const {transaction} = data;
+
+        console.log(transaction)
+        setTransactions([
+            ...transactions,
+            transaction
+        ]);
+    }
+
+
+    return (
+        <TransactionsContext.Provider value={{transactions, createTransaction}}>
+            {children}
+        </TransactionsContext.Provider>
+    )
+
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context
+}
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+import { createServer, Model} from 'miragejs';
+
+createServer({
+
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freela Software',
+          type: 'deposit',
+          category: 'Service',
+          amount: 6000,
+          createdAt: new Date('2121-10-12 00:46:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'contas',
+          amount: 2000,
+          createdAt: new Date('2121-10-13 09:40:00')
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace= 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema,request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +53,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
